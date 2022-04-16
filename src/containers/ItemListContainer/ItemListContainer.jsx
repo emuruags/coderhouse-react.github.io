0e96096ef2b5cfc5c3e6af4872332bded1c315a5
emuruags <96442652+emuruags@users.noreply.github.com>
2022-04-16 12:38:56 -0300

    Update ItemListContainer.jsx
    fix gif loading

--- src/containers/ItemListContainer/ItemListContainer.jsx
+++ src/containers/ItemListContainer/ItemListContainer.jsx
@@ -29,7 +29,9 @@ function ItemListContainer( ) {
   return (
     <>
         {
-          loading   ? <img className="w-45 mx-auto " src="./img/loading.gif" alt="loading" />
+          loading   ? <img className="w-45 mx-auto " 
+                          src="https://firebasestorage.googleapis.com/v0/b/coderhouse-reactjs-ecommerce.appspot.com/o/img%2Floading%2Floading.gif?alt=media&token=290b2e66-747c-4f5b-b784-bf84bfc8cf0a" 
+                        alt="loading" />
                     : <Container>
                           <div className="row align-items-start m-3"> 
                               <ItemList prods={ prods } /> 
